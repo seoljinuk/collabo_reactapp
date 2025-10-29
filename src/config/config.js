8811a3839a002a8218995b04c42ae3942800a374
bluesky <bluesky@example.com>
2025-10-29 10:51:51 +0900

    react 2052/10/29

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,7 @@
 // 이 파일은 설정용 파일입니다.
-const API_HOST = "localhost";  // 호스트 컴퓨터 이름(127.0.0.1)
+//const API_HOST = "localhost";  // 호스트 컴퓨터 이름(127.0.0.1)
+// const API_HOST = "43.203.83.182" // EC2 공인 IP 주소
+const API_HOST = "ugcadman.shop" // 로드 밸런스 사용시 나의 도메인 정보
 
 const API_PORT = "9000"; // 스프링 부트 포트 번호
 
